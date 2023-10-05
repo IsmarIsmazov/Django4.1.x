1f2a57dc3b4b36325f5dc69100f28937c12ff1fc
Ismar <ismar110501@gmail.com>
2023-10-05 15:02:56 +0600

    rename from info to func and core

--- func/core/asgi.py
+++ func/core/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for info project.
+ASGI config for func project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'info.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'func.settings')
 
 application = get_asgi_application()
